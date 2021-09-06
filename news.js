11b20889131751b895b870490240bda8226fc4ad
Paul AI <pasukwo5@gmail.com>
2021-09-06 19:37:34 +0100

    formate documated code

--- news.js
+++ news.js
@@ -1,6 +1,6 @@
-let jsondata = []
+let jsondata = [];
 
-const app = async() =>{
+const app = async () => {
   document.body.innerHTML = `<div class="loader text-center">
     <span class="span"></span>
 </div>`;
@@ -8,19 +8,19 @@ const app = async() =>{
     const fetching = await fetch(
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=3fbcb81669864b94af12e1432ae54e46"
     );
-    let data = await fetching.json()
-    jsondata = data
+    let data = await fetching.json();
+    jsondata = data;
     getData(jsondata.articles);
   } catch (error) {
     console.log(error);
-    
   }
-}
+};
 
-const getData = (data) => {
-    const finallData = data.map((item, key) => {
-        console.log(item);
-        return `<div class="card border w-50 mt-3 mx-auto" style="width: 18rem;">
+const getData = data => {
+  const finallData = data
+    .map((item, key) => {
+      console.log(item);
+      return `<div class="card border w-50 mt-3 mx-auto" style="width: 18rem;">
   <img src=${item.urlToImage} class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${item.title}</h5>
@@ -28,9 +28,9 @@ const getData = (data) => {
     <a href=${item.url} target="_blank" class="btn btn-primary">Read More</a>
   </div>
 </div>`;
-    }).join('');
-    document.body.innerHTML = finallData; 
+    })
+    .join("");
+  document.body.innerHTML = finallData;
+};
 
-}
-
-app()
+app();
